638b5d7be81dc45b3eac29d9c3f02b83a567f050
NEH <erik@bodigiti.com>
2020-02-28 18:28:10 -0500

    AJAX Vanilla: Cleaned up a bit

--- themes/loveyou/src/js/form.js
+++ themes/loveyou/src/js/form.js
@@ -35,91 +35,42 @@ var serialize = function (form) {
 
 };
 
-// MOZILLA
-function logSubmit(event) {
-  log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-  event.preventDefault();
-}
-
-const form = document.getElementById('contact');
-const log = document.getElementById('serverResponse');
-form.addEventListener('submit', logSubmit);
-
 /**
- * Ajax Vanilla
+ * AJAX Vanilla
  */
 
-function listenTest(formID, request) {
+function listenFormSubmit(formID, request) {
   var form = document.querySelector(formID);
   if (form.addEventListener) {
-    var added = form.addEventListener; console.log("added $$$$$$$$$$$ ", added);
-    form.addEventListener("submit", request, false);  //Modern browsers
+    form.addEventListener("submit", request, false);
   }
 }
 
-function youTube(form) {
-  form.preventDefault();
-//  console.log("form 11111111111111111111111111 ", form);
-//  var form = document.querySelector('#contact');
+// Success Message Function (called after AJAX Success)
+function successNotice(jsClass) {
+  let elementSuccess = document.querySelectorAll(jsClass);
+  $(elementSuccess).show(0).delay(5000).hide(0);
+}
+
+function ajaxRequest(event) {
+  event.preventDefault(); // stop submit so input values do not get cleared
+  // FIXME make form id dynamic 
   var form = document.querySelector('#contact');
-  console.log("Object.values $$$$$$$$$$$$$$$$$", Object.values(form));
 
   var formData = serialize(form);
-  console.log("formData $$$$$$$$$$$$$$$$$$$$$$$$ ", formData);
   var xhr = new XMLHttpRequest();
 
   xhr.onload = function() {
-    var serverResponse = document.getElementById("serverResponse");
-    serverResponse.innerHTML = this.responseText;
+    successNotice(".js-success.contact");
   }
 
   xhr.open("POST", 'https://us-central1-loveyou-forms.cloudfunctions.net/formHandler');
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  //xhr.send("app=AJAXMGCXSg6fbLgteaWDLnTwL2EC3Kj7y4kDWqGU4Vzcq8UQKAzfZvJ4xkjTv8GjXKvdEs6BHGjU");
   xhr.send(formData);
 }
-//youTube();
-listenTest('#contact', youTube);
-//////////////
 
-function ajaxToo(form) {
-  var formData = new FormData(form);
-  console.log("formData $$$$$$$$$$$$$$$$$$$ ", formData);
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (request.readyState == request.DONE && request.status == 200) {
-        alert(request.responseText);
-    }
-  };
-  request.open("POST", 'https://us-central1-loveyou-forms.cloudfunctions.net/formHandler');
-  request.send(formData);
-}
+listenFormSubmit('#contact', ajaxRequest);
 
-//listenTest('#contact', ajaxToo);
-
-///////////////////////////////////
-
-function ajaxMe(string) {
-  // Request
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://us-central1-loveyou-forms.cloudfunctions.net/formHandler');
-  xhr.send(string);
-
-  // Response
-  xhr.onreadystatechange = function () {
-    var DONE = 4; // readyState 4 means the request is done.
-    var OK = 200; // status 200 is a successful return.
-    if (xhr.readyState === DONE) {
-      if (xhr.status === OK) {
-        console.log(xhr.responseText); // 'This is the returned text.'
-      } else {
-        console.log('Error: ' + xhr.status); // An error occurred during the request.
-      }
-    }
-  }
-};
-
-//listentest('#contact', ajaxme);
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -162,7 +113,9 @@ function submitFormContact(formContact){
 
 // Contact Form: Function Call
 // first parameter selects form to listen to, second parameter is function to attach to form
+//
 // listenForm('#contact', submitFormContact);
+//
 
 //
 //
