13646835e67014e8ee990b10725e95158b777706
NEH <erik@bodigiti.com>
2020-02-29 10:28:27 -0500

    Browserslist: Add IE11 ignore to package.json

--- themes/loveyou/src/js/form.js
+++ themes/loveyou/src/js/form.js
@@ -12,15 +12,14 @@ function listenFormSubmit(formID, request) {
 // Success Message Function (called after AJAX Success)
 function successNotice(jsClass) {
   let elementSuccess = document.querySelectorAll(jsClass);
-  $(elementSuccess).show(0).delay(5000).hide(0);
+  $(elementSuccess).show(0); // to hide after 5 seconds append .delay(5000).hide(0)
 }
 
 function ajaxRequest(event) {
   event.preventDefault(); // stop submit so input values do not get cleared
   // FIXME make form id dynamic 
   var form = document.querySelector('#contact');
-  //  var formData = serialize(form);
-  // Serialize form, format must be:
+  // Serialize form, format must be string:
   // name=Jimmy Flash&phone=999.555.1212&email=jimmy@flash.com&message=Hey, how are you doing?&template=contactDefault& etc...
   // ie11 cannot use Object.values and babel is not transpiling it
   var formData = Object.values(form).reduce((string, field) => { 
